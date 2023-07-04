cd9b821ed6f8608515bfa61af2adf9e7b6aaee1e
root <root@OMENOfLxy.localdomain>
2023-07-04 10:31:30 +0800

    11

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -1,6 +1,6 @@
 package raft
 
-//
+// export PATH=$PATH:/usr/local/go/bin
 // this is an outline of the API that raft must expose to
 // the service (or tester). see comments below for
 // each of these functions for more details.
